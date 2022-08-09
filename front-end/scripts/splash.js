a9fe2e5a32596f1af651d5f2050a5e5aebf84d76
Ian <qaz7456@gmail.com>
2022-08-09 18:23:32 +0800

    refactor: adjust IIFE
    Remove cumbersome code to make it easier to read.
    
    Adjust content:
    1. splash.js:
       - Flatten method (getRandomIndex, loadSplash) logic to IIFE.

--- front-end/scripts/splash.js
+++ front-end/scripts/splash.js
@@ -1,19 +1,10 @@
 (function () {
-    let splash = ['../front-end/images/sleep.gif', '../front-end/images/rain.gif'];
-
-    function loadSplash() {
-        const index = getRandomIndex();
-        document.querySelectorAll('img').forEach(function (tag) {
-            tag.src = splash[index];
-        });
-        document.querySelectorAll('label').forEach(function (tag) {
-            tag.innerHTML = index == 0 ? '壓縮雞努力起床中Zzzz...' : '再忙也要休息一下';
-        });
-    }
-
-    function getRandomIndex() {
-        return Math.floor(Math.random() * splash.length);
-    }
-
-    loadSplash();
+    const splash = ['../front-end/images/sleep.gif', '../front-end/images/rain.gif'];
+    const index = Math.floor(Math.random() * splash.length);
+    document.querySelectorAll('img').forEach(function (tag) {
+        tag.src = splash[index];
+    });
+    document.querySelectorAll('label').forEach(function (tag) {
+        tag.innerHTML = index == 0 ? '壓縮雞努力起床中Zzzz...' : '再忙也要休息一下';
+    });
 })();
