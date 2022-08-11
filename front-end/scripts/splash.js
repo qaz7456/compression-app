8c7d404fa7c119c55dcc4badeb7a12ab0eaa1870
Ian <qaz7456@gmail.com>
2022-08-11 11:23:35 +0800

    refactor: replacing vanilla JS with jQuery

--- front-end/scripts/splash.js
+++ front-end/scripts/splash.js
@@ -4,12 +4,10 @@
     const userLang = navigator.language || navigator.userLanguage || ''; 
     const isZh = userLang.includes('zh');
     const isCht = isZh ? ['zh-TW', 'zh-CHT'].includes(userLang) : isZh;
-    document.querySelectorAll('img').forEach(function (tag) {
-        tag.src = splash[index];
-    });
-    document.querySelectorAll('label').forEach(function (tag) {
-        tag.innerHTML = index == 0 ? 
-            isZh ? isCht ? '壓縮雞努力起床中Zzzz...' : '压缩鸡努力起床中Zzzz...' : 'Try to get up Zzzz...' : 
-            isZh ? '再忙也要休息一下' : 'Life is but a span';
-    });
+    $('img').attr('src', splash[index]);
+    $('label').html(
+        index == 0 ? 
+        isZh ? isCht ? '壓縮雞努力起床中Zzzz...' : '压缩鸡努力起床中Zzzz...' : 'Try to get up Zzzz...' : 
+        isZh ? '再忙也要休息一下' : 'Life is but a span'
+    );
 })();
